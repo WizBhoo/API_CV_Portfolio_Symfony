9ead3286795a513dfa6e8cb71c1ccb9dc6c4772f
Adrien PIERRARD <adrien.pierrard@gmail.com>
2020-08-25 14:38:49 +0200

    feat(mailer): added mailer system and implemented it to contact form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,9 +8,11 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use App\Mailer\MailerMessage;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 /**
  * Class ContactController.
@@ -20,17 +22,21 @@ class ContactController extends AbstractController
     /**
      * Show contact page with contact form.
      *
-     * @param Request $request
+     * @param Request       $request
+     * @param MailerMessage $mailer
      *
      * @return Response
+     *
+     * @throws TransportExceptionInterface
      */
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerMessage $mailer): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $mailer->sendContactEmail($contact);
             $this->addFlash(
                 'success',
                 'Your message has been sent !'
